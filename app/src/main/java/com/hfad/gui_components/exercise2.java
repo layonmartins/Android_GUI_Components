fd4fe54be44377f6207eb0529c510d332d06663f
Layon Martins Fonseca <layonmartins03@gmail.com>
2020-01-18 15:58:35 -0300

    exercise 2

---
+++ app/src/main/java/com/hfad/gui_components/exercise2.java
@@ -0,0 +1,14 @@
+package com.hfad.gui_components;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class exercise2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise2);
+    }
+}
